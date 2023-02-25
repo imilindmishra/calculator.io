74657de9086be1e25170e03c666e4770c083a8c8
Milind Mishra <113526126+imilindmishra@users.noreply.github.com>
2023-02-25 22:47:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function dark(){
+    document.getElementById("theme").setAttribute('href', 'dark.css');
+ }
+ function nature(){
+    document.getElementById("theme").setAttribute('href', 'nature.css');
+}
